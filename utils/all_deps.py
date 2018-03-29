664dde6c083f3f3e617d029b0e52d49f9b24bf0c
Jeffrey Elms <elmsfu@freshred.net>
2018-03-29 00:28:53 -0700

    utils: generate verilog dependencies in python generator
    Also cleaning up some cruft
    
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- utils/all_deps.py
+++ utils/all_deps.py
@@ -7,8 +7,11 @@ import sys
 
 import deps_verilog
 import deps_xml
-
+from n import TEMPLATE_PREFIX
 import listfiles
+TOPDIR = listfiles.TOPDIR
+
+BASEMUX_FMT = os.path.join(listfiles.TOPDIR, 'vpr/muxes/{muxtype}/mux{width}/mux{width}.sim.v')
 
 from lib.asserts import assert_eq
 
@@ -42,6 +45,10 @@ mux_gen_keys = [MUX_TYPE_KEY,
 
 MUX_TYPE_ROUTING = 'routing'
 MUX_TYPE_LOGIC = 'logic'
+
+MUX_MAKEFILE = 'Makefile.mux'
+N_MAKEFILE = 'Makefile.N'
+V2X_MAKEFILE = 'Makefile.v2x'
 
 SIMV_SUFFIX = 'sim.v'
 PB_SUFFIX = 'pb_type.xml'
@@ -138,30 +145,39 @@ def mux_gen_check_args(makefile):
 
   return opts
 
-def mux_gen_deps(makefile):
+def mux_gen_deps(muxdir):
   """generate build rule for mux generation"""
-  fmt = """# mux generation rul
-{outputs}: {makefile} {cmd}
+  fmt = """# mux generation rule
+{outputs}: {makefile} {cmd} {basemux}
 \t$(QUIET)cd {directory} && {cmd} {args}
 MUX_OUTPUTS += {outputs}
 """
-  out_dir = os.path.dirname(makefile)
+  makefile = os.path.join(muxdir, MUX_MAKEFILE)
+  out_dir = muxdir
   opts = mux_gen_check_args(makefile)
   outfile = opts[MUX_OUTFILE_KEY]
   outputs = [os.path.join(out_dir, outfile + '.' + suffix) for suffix in EXPAND_SUFFIXES]
   CMD = os.path.realpath('mux_gen.py')
-  rule = fmt.format(target=outfile, outputs=' '.join(outputs), makefile=makefile, directory=out_dir, cmd=CMD, args=mux_gen_args(opts, out_dir))
+  # TODO: Revisit if we should use logic vpr mux for generated routing muxes
+  muxtype = MUX_TYPE_LOGIC #opts[MUX_TYPE_KEY]
+  basemux = BASEMUX_FMT.format(muxtype=muxtype, width=opts[MUX_WIDTH_KEY])
+  logging.debug('basemux for %s: %s', makefile, basemux)
+
+  rule = fmt.format(target=outfile, outputs=' '.join(outputs),
+                    makefile=makefile, directory=out_dir, cmd=CMD,
+                    args=mux_gen_args(opts, out_dir), basemux=basemux)
+
   return rule, outputs
 
-def gen_rules_Ntemplate(makefile, base):
+def gen_rules_Ntemplate(in_dir, base):
   fmt = """#template expansion rule
 {output}: {template} {makefile} {cmd}
 \t$(QUIET){cmd} {template} {output}
 N_OUTPUTS += {output}
 """
   CMD = os.path.realpath('n.py')
-  in_dir = os.path.dirname(makefile)
-  out_dir = os.path.dirname(makefile)
+  makefile = os.path.join(in_dir, N_MAKEFILE)
+  out_dir = in_dir
   values_str = open(makefile,'r').read().strip()
   eqn = [xx.strip() for xx in values_str.split('=')]
   assert_eq(eqn[0], 'NTEMPLATE_VALUES')
@@ -169,18 +185,21 @@ N_OUTPUTS += {output}
   logging.debug('%s-> %s', values_str, nvalues)
   res = ''
   outputs = []
-  for n in nvalues:
-    for ftype in EXPAND_SUFFIXES:
-      template_name = base.format(N='N')
-      outname = base.format(N=n)
-      template = os.path.join(in_dir, 'ntemplate.{name}.{ftype}'.format(name=template_name, ftype=ftype))
-      output = os.path.join(out_dir, '{name}.{ftype}'.format(name=outname, ftype=ftype))
-      logging.debug('generating rule %s: %s', output, template)
-      res += fmt.format(output=output, template=template, cmd=CMD, makefile=makefile)
-      outputs.append(output)
+  ftype = SIMV_SUFFIX
+  for nv in nvalues:
+    template_name = base.format(N='N')
+    outname = base.format(N=nv)
+    template = os.path.join(in_dir,
+                            '{prefix}{name}.{ftype}'.format(prefix=TEMPLATE_PREFIX,
+                                                            name=template_name, ftype=ftype))
+    output = os.path.join(out_dir, '{name}.{ftype}'.format(name=outname, ftype=ftype))
+    logging.debug('generating rule %s: %s', output, template)
+    res += fmt.format(output=output, template=template, cmd=CMD, makefile=makefile)
+    outputs.append(output)
+
   return res, outputs
 
-def gen_rules_v2xml(makefile):
+def gen_rule_v2xml(simv, makefile=''):
   fmt = """# verilog to xml expansion
 {pb_output}: {simv} {pb_cmd} {makefile}
 \t$(QUIET){pb_cmd} {extra_args} {pb_args} {simv}
@@ -188,23 +207,21 @@ def gen_rules_v2xml(makefile):
 \t$(QUIET){model_cmd} {extra_args} {model_args} {simv}
 V2X_OUTPUTS += {pb_output} {model_output}
 """
-  curdir = os.path.dirname(makefile)
   dict_args = {}
   dict_args['makefile'] = makefile
   dict_args['pb_cmd'] = os.path.realpath('vlog/vlog_to_pbtype.py')
   dict_args['model_cmd'] = os.path.realpath('vlog/vlog_to_model.py')
-
-  simvlist = [xx for xx in os.listdir(curdir) if xx.endswith(SIMV_SUFFIX)]
-  assert_eq(len(simvlist), 1)
-  dict_args['simv'] = os.path.join(curdir, simvlist[0])
+  dict_args['simv'] = simv
   base = dict_args['simv'][:-len(SIMV_SUFFIX)]
 
   extra_args = ''
-  values_str = open(makefile,'r').read().strip()
-  if len(values_str) > 0:
-    eqn = [xx.strip() for xx in values_str.split('=')]
-    assert_eq(eqn[0], 'TOP_MODULE')
-    extra_args += '--top {}'.format(eqn[1])
+  if makefile:
+    values_str = open(makefile,'r').read().strip()
+    if len(values_str) > 0:
+      eqn = [xx.strip() for xx in values_str.split('=')]
+      assert_eq(eqn[0], 'TOP_MODULE')
+      extra_args += '--top {}'.format(eqn[1])
+
   dict_args['extra_args'] = extra_args
   dict_args['pb_output'] = base + PB_SUFFIX
   dict_args['model_output'] = base + MODEL_SUFFIX
@@ -213,6 +230,24 @@ V2X_OUTPUTS += {pb_output} {model_output}
 
   rule = fmt.format(**dict_args)
   outputs = [dict_args['model_output'], dict_args['pb_output']]
+
+  return rule, outputs
+
+def gen_rules_v2xml(in_dir):
+  fmt = """# verilog to xml expansion
+{pb_output}: {simv} {pb_cmd} {makefile}
+\t$(QUIET){pb_cmd} {extra_args} {pb_args} {simv}
+{model_output}: {simv} {pb_cmd} {makefile}
+\t$(QUIET){model_cmd} {extra_args} {model_args} {simv}
+V2X_OUTPUTS += {pb_output} {model_output}
+"""
+  makefile = os.path.join(in_dir, V2X_MAKEFILE)
+
+  simvlist = [xx for xx in os.listdir(in_dir) if xx.endswith(SIMV_SUFFIX) and not xx.startswith(TEMPLATE_PREFIX)]
+  logging.debug(simvlist)
+  assert_eq(len(simvlist), 1)
+
+  rule, outputs = gen_rule_v2xml(os.path.join(in_dir, simvlist[0]), makefile)
 
   return rule, outputs
 
@@ -229,29 +264,26 @@ clean: {type}-clean
     res += clean_template.format(type=make_type)
   return res
 
+def get_dirs_with_file(fname):
+  return {os.path.dirname(xx) for xx in listfiles.listfiles([TOPDIR],[]) if xx.endswith(fname)}
+
 def gen_deps(ff):
   # find mux gen
   mux_outputs = []
-  muxes = [xx for xx in listfiles.listfiles([listfiles.TOPDIR],[]) if xx.endswith('Makefile.mux')]
-  for mux in muxes:
-    logging.debug('mux: %s', mux)
-    rule, outputs = mux_gen_deps(mux)
+  muxdirs = get_dirs_with_file(MUX_MAKEFILE)
+  for muxdir in muxdirs:
+    logging.debug('mux: %s', muxdir)
+    rule, outputs = mux_gen_deps(muxdir)
     ff.write(rule)
     mux_outputs += outputs
 
-  # generate template, verilog, and xml dependency ruls
-  templates = [xx for xx in listfiles.listfiles([listfiles.TOPDIR],[]) if xx.endswith('Makefile.N')]
-  v2xml = [xx for xx in listfiles.listfiles([listfiles.TOPDIR],[]) if xx.endswith('Makefile.v2x')]
-  v2x_outputs = []
-  for makefile in v2xml:
-    logging.debug('generating rules for %s', makefile)
-    rules, outputs = gen_rules_v2xml(makefile)
-    ff.write(rules)
-    v2x_outputs += outputs
+  template_dirs = get_dirs_with_file(N_MAKEFILE)
+  v2xml_dirs = get_dirs_with_file(V2X_MAKEFILE)
+  v2xml_nontemplate = v2xml_dirs.difference(template_dirs)
 
   # generate and traditional are mutually exclusive
-  gend = set(map(os.path.dirname, templates)).intersection(set([os.path.dirname(x) for x in muxes]))
-  trad = set(map(os.path.dirname, templates)).difference(set([os.path.dirname(x) for x in muxes]))
+  gend = template_dirs.intersection(muxdirs)
+  trad = template_dirs.difference(muxdirs)
   logging.debug('generated %s', gend)
   logging.debug('traditional %s', trad)
 
@@ -260,7 +292,7 @@ def gen_deps(ff):
   for dirname in trad:
     name = os.path.basename(dirname).replace('N', '{N}')
     logging.debug('expanding templates %s', name)
-    rules, outputs = gen_rules_Ntemplate(os.path.join(dirname, 'Makefile.N'), name)
+    rules, outputs = gen_rules_Ntemplate(dirname, name)
     ff.write(rules)
     N_outputs += outputs
 
@@ -268,21 +300,41 @@ def gen_deps(ff):
   Ngen_outputs = []
   for dirname in gend:
     opts = mux_gen_check_args(os.path.join(dirname, 'Makefile.mux'))
-    name = opts[MUX_OUTFILE_KEY].replace('N', '{N}')
+    name = opts[MUX_OUTFILE_KEY].replace('N', '{N}').replace(TEMPLATE_PREFIX,'')
     logging.debug('expanding generated templates %s', name)
-    rules, outputs = gen_rules_Ntemplate(os.path.join(dirname, 'Makefile.N'), name)
+    rules, outputs = gen_rules_Ntemplate(dirname, name)
     ff.write(rules)
     Ngen_outputs += outputs
 
-  all_outputs = mux_outputs + v2x_outputs + N_outputs + Ngen_outputs
-  logging.debug('.v outputs: %s', [xx for xx in all_outputs if xx.endswith(SIMV_SUFFIX)])
+  # generate and xml dependency rules
+  v2x_outputs = []
+  for v2xmldir in v2xml_nontemplate:
+    logging.debug('generating rules for %s', v2xmldir)
+    rules, outputs = gen_rules_v2xml(v2xmldir)
+    ff.write(rules)
+    v2x_outputs += outputs
+  for fname in Ngen_outputs:
+    logging.debug('generating rules for %s', fname)
+    rules, outputs = gen_rule_v2xml(fname)
+    ff.write(rules)
+    v2x_outputs += outputs
 
-  # mux output .v files only depend on mux{width} which are not generated
+  all_outputs = mux_outputs + v2x_outputs + N_outputs + Ngen_outputs
+  v_generated = [xx for xx in all_outputs if xx.endswith(SIMV_SUFFIX)]
+  logging.debug('.v outputs: %s', v_generated)
+
+  # mux output .v files only depend on mux<width>.sim.v which are not generated
   # N outputs sim.v depend on the template sim.v on which we can generate dependencies
 
   # find all v files and add Makefile.mux dirs
   # generate verilog dependency rules
-  #deps_verilog.gen_deps('', '{from_file}: {on_file}\n')
+  vfiles = [xx for xx in listfiles.listfiles([TOPDIR],[]) if xx.endswith(SIMV_SUFFIX)]
+  # remove any generated files
+  vfiles = set(vfiles).difference(v_generated)
+  for vfile in vfiles:
+    data = deps_verilog.gen_dep(open(vfile, 'r'), '{from_file}: {on_file}\n')
+    logging.debug('verilog dependency %s\n"%s"', vfile, data.getvalue())
+    ff.write(data.getvalue())
 
   # generated xml files depend on a .v which should define the correct dependencies
   # unless there is an xml include that points to a non-generated .xml that sits next to a non-generated sim.v
