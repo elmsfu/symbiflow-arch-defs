244c13397b38970fabc0a86e3999f805ab2f2962
Jeffrey Elms <elmsfu@freshred.net>
2018-03-29 12:43:54 -0700

    utils: use lxml for xml dependency
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- utils/deps_xml.py
+++ utils/deps_xml.py
@@ -4,12 +4,14 @@ Generate a Makefile .d fragment for the XML includes.
 """
 
 import argparse
+import logging
 import os
-import re
 import sys
 
 from io import StringIO
+from lxml import etree as ET
 
+from lib.argparse_extra import ActionStoreBool
 from lib.deps import add_dependency
 from lib.deps import write_deps
 
@@ -19,25 +21,30 @@ parser.add_argument(
     "inputfile",
     type=argparse.FileType('r'),
     help="Input XML file")
-
-
-xi_include = re.compile('<xi:include[^>]*href="([^"]*)"')
+parser.add_argument(
+    "--verbose",
+    action=ActionStoreBool, default=os.environ.get('V', '')=='1',
+    help="Be Verbose. Print lots of information.")
 
 def gen_deps(inputfile, fmt=None):
     inputpath = os.path.abspath(inputfile.name)
     inputdir = os.path.dirname(inputpath)
 
     data = StringIO()
-
-    matches = set(xi_include.findall(inputfile.read()))
-    for includefile in matches:
-        includefile_path = os.path.abspath(os.path.join(inputdir, includefile))
-        add_dependency(data, inputpath, includefile_path, fmt)
+    tree = ET.parse(inputfile)
+    for el in tree.iter():
+        if str(el.tag).endswith('XInclude}include'):
+          includefile_path = os.path.abspath(os.path.join(inputdir, el.get('href')))
+          logging.info('Adding dep: %s %s', inputpath, includefile_path)
+          add_dependency(data, inputpath, includefile_path, fmt)
 
     return data
 
 def main(argv):
     args = parser.parse_args(argv[1:])
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
     data = gen_deps(args.inputfile)
     write_deps(args.inputfile.name, data)
 
