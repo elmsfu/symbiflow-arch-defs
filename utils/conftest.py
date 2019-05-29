15f6b9795974ab75a7ab7860281fcaf729bed8c5
Elms <elmsfu@freshred.net>
2019-05-29 15:07:17 -0700

    WIP: rm round2

--- utils/conftest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-collect_ignore = [
-    "vlog/vlog_to_model.py",  # Can't be imported - Issue #61
-    "vlog/vlog_to_pbtype.py",  # Can't be imported - Issue #61
-]
